2cb1addaad67556031dbb05195816ac4d5227b16
YouGuavas <patrickyambrick@gmail.com>
2018-10-22 12:53:17 -0400

    Make environment variable

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import TwitterLogin from 'react-twitter-auth';
+const environment = 'prod';
+let API_URL;
+
+environment === 'dev' ? API_URL = 'http://localhost:3333' : API_URL = '';
+
 
 export default class Nav extends Component {
 	handleClick = () => {
@@ -34,7 +39,7 @@ export default class Nav extends Component {
 							{this.props.isAuthed === true ? <a href={`https://twitter.com/${this.props.user.username}`} target='_blank' id='twitterUser'><img className='twitterAvi' src={this.props.user.picture}/>@{this.props.user.username}</a> : null}
 							<Link to='/' onClick={this.handleClickItem} className='navbar-item'>Home</Link>
 							{this.props.isAuthed === true ? <Link to='/newbook' onClick={this.handleClickItem} className='navbar-item'>New Book</Link> : null}
-							{this.props.isAuthed === true ? <Link to='/' onClick={this.logout} className='navbar-item'>Logout</Link> : <TwitterLogin className='navbar-item twitterer' loginUrl='/api/auth/twitter' onFailure={this.handleFail} onSuccess={this.handleSuccess} requestTokenUrl='/api/auth/twitter/reverse' /> }
+							{this.props.isAuthed === true ? <Link to='/' onClick={this.logout} className='navbar-item'>Logout</Link> : <TwitterLogin className='navbar-item twitterer' loginUrl={`${API_URL}/api/auth/twitter`} onFailure={this.handleFail} onSuccess={this.handleSuccess} requestTokenUrl={`${API_URL}/api/auth/twitter/reverse`} /> }
 						</div>
 					</div>
 				</nav>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,27 +1,33 @@
 import axios from 'axios';
+const environment = 'prod';
 
-//const BASE_URL = 'http://localhost:3333';
+let BASE_URL; 
+
+environment === 'dev' ? BASE_URL = 'http://localhost:3333' : BASE_URL = '';
+
 export {getBooksData, newBook, getBookData, deleteBook, newComment};
 
 function deleteBook(bookID) {
-	const url = `/api/deletebook/${bookID}`;
+	const url = `${BASE_URL}/api/deletebook/${bookID}`;
 	return axios.get(url).then(res=>res);
 };
 
 function newComment(bookID, comment) {
-	const url = `/api/newcomment/${bookID}`;
+	const url = `${BASE_URL}/api/newcomment/${bookID}`;
 	return axios.post(url, {newComment: comment}).then(res=>res);
-}
+};
 
 function getBooksData() {
-	const url = `/api/books`;
+	const url = `${BASE_URL}/api/books`;
 	return axios.get(url).then(res=>res.data);
 };
+
 function getBookData(bookID) {
-	const url = `/api/book/${bookID}`;
+	const url = `${BASE_URL}/api/book/${bookID}`;
 	return axios.get(url).then(res=>res);
 };
+
 function newBook(data) {
-	const url = `/api/newbook`;
+	const url = `${BASE_URL}/api/newbook`;
 	return axios.post(url, {title: data.title, author: data.author, published: data.published, finished: data.finished, synopsis: data.synopsis, notes: data.notes}).then(res=>res.data);
 };
