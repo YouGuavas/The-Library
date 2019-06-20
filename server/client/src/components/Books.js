3b2f144249cde7f519b1f74b0cb0ea2a6b034c6d
Patrick Yambrick <patrickyambrick@gmail.com>
2019-06-20 14:54:38 -0400

    Fix pagination current display and pad empty rows on last page

--- server/client/src/components/Books.js
+++ server/client/src/components/Books.js
@@ -25,51 +25,54 @@ export default class Books extends Component {
 			});
 		});
 	}
-	setPage = (page) => {
-		this.setState({
-			page: page
-		}, () => {
-				this.setState({
-					displayBooks: this.state.books.slice(this.state.page*this.state.perPage, this.state.page*this.state.perPage+this.state.perPage)
-				})
-			});
-	}
 	incrementPage = () => {
-		if (this.state.page < this.state.numPages - 1) (
+		if (this.state.page < this.state.numPages - 1) {
+		this.togglePage(this.state.page);
+		this.togglePage(this.state.page+1);
 		this.setState({
 			page: this.state.page+1
-		}, () => {
-				this.setState({
-					displayBooks: this.state.books.slice(this.state.page*this.state.perPage, this.state.page*this.state.perPage+this.state.perPage)
-				})
-			})
-		);
+		}, () => {this.displayBooks()})
+		}
 	}
 	decrementPage = () => {
-		if (this.state.page > 0) (
+		if (this.state.page > 0) {
+		this.togglePage(this.state.page);
+		this.togglePage(this.state.page-1);
 		this.setState({
 			page: this.state.page-1
-		}, () => {
-				this.setState({
-					displayBooks: this.state.books.slice(this.state.page*this.state.perPage, this.state.page*this.state.perPage+this.state.perPage)
-				})
-			}) 
-		);
+		}, () => {this.displayBooks()}) 
+		}
 	}
+	displayBooks = () => {
+		this.setState({
+			displayBooks: this.state.books.slice(this.state.page*this.state.perPage, this.state.page*this.state.perPage+this.state.perPage)
+		}, () => {
+			if (this.state.displayBooks.length < this.state.perPage) {
+				let books = this.state.displayBooks;
+				console.log(this.state.displayBooks.length);
+				for (let i = this.state.displayBooks.length; i < this.state.perPage; i++) {
+					books.push({'title': '', 'author': '', '_id': ''})
+				};
+				this.setState({displayBooks: books});
+			}
+		})
+	}
+
+	setPage = (prevPage, newPage) => {
+		this.togglePage(prevPage);
+		this.togglePage(newPage);
+		this.setState({
+			page: newPage
+		}, () => {this.displayBooks()});
+	}
+	togglePage = (page) => {
+		document.getElementById(page).classList.toggle('is-current');
+	} 
 	componentDidMount() {
 		this.getBooks();
 	}
 	render() {
 		const books = this.state.displayBooks;
-		const {numPages} = this.state;
-		let links = [];
-		for (let i = 0; i < numPages; i++) {
-			links.push(
-				<li key={i}>
-					<button className='pagination-link' id={i} onClick={() => this.setPage(i)}>{i+1}</button>
-				</li>
-				)
-		}
 		return(
 			<div className='books'>
 				<div className='level' id='title-level'>
@@ -83,34 +86,36 @@ export default class Books extends Component {
 					</div>
 				</div>
 				{books.map((book, index) => (
-					<Item title={book.title} author={book.author || 'unknown'} url={book._id} key={index}/>
+					<Item title={book.title} author={book.author} url={book._id} key={index}/>
 				))
 				}
-			<Paginator tabindex='0' decrementPage={this.decrementPage} incrementPage={this.incrementPage} links={links}/>
+			<Paginator tabindex='0' setPage={(pP, nP) => {this.setPage(pP, nP)}} decrementPage={this.decrementPage} incrementPage={this.incrementPage} numPages={this.state.numPages}/>
 			</div>
 			)
 	}
 }
 class Paginator extends Component {
-	constructor() {
-		super();
-		this.state = {
-			currentPage: 0
+	createLinks = () => {
+		const links = [];
+		for (let i = 0; i < this.props.numPages; i++) {
+			links.push(
+				[]
+				)
 		}
-	}
-	setCurrent = () => {
-		document.getElementById(this.state.currentPage).setAttribute('class', 'pagination-link is-current')
+		return links;
 	}
 	render() {
+		const links = this.createLinks();
 		return (
 			<nav className='pagination is-centered'>
 				<button className='pagination-previous' onClick={this.props.decrementPage}>Previous</button>
 				<button className='pagination-next' onClick={this.props.incrementPage}>Next</button>
 				<ul className='pagination-list'>
-				{
-					this.props.links.map((item) => (
-						item
-						)) 
+				{ links.map((item, i) => (
+					<li key={i}>
+						{i === 0 ? <button className='pagination-link is-current' id={`${i}`} onClick={() => this.props.setPage(document.getElementsByClassName('is-current')[0].id, i)}>{i+1}</button> : <button className='pagination-link' id={`${i}`} onClick={() => this.props.setPage(document.getElementsByClassName('is-current')[0].id, i)}>{i+1}</button> }
+					</li>
+					))
 					}
 				</ul>
 			</nav>
@@ -122,15 +127,17 @@ class Paginator extends Component {
 class Item extends Component {
 	render() {
 		return(
-			<div className='level-books level'>
-				<Link to={`/book/${this.props.url}`} className='level-item'>
+			<div className={this.props.title.length > 0 ? 'level-books level' : 'level'}>
+				{this.props.title.length > 0 ? (<Link to={`/book/${this.props.url}`} className='level-item'>
 						<div className='has-text-centered column'>
 							{this.props.title}
+							{this.props.title.length === 0 ? <span className='space-taker'></span> : null}
 						</div>
 						<div className='has-text-centered hideWhenSmall column'> 
 							{this.props.author}
 						</div>
-				</Link>
+					{this.props.title.length === 0 ? <span className='space-taker'></span> : null}
+		</Link> ) : <span className='space-taker'></span> }
 			</div>
 			)
 	}
