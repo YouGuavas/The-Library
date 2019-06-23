b07978209e63a8e34afce060bd180c1408f49f23
Patrick Yambrick <patrickyambrick@gmail.com>
2019-06-22 20:45:36 -0400

    Remove create-react-app junk

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "client", filePath)))
 
 app.use('/api', routes);
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", 'public',/*"dist",*/ "index.html"));
+    res.sendFile(path.join(__dirname, "client", 'public',filePath, "index.html"));
 });
 app.listen(port, () => {
 	console.log(`Your app is now running on port: ${port}`);
