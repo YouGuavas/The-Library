180c2f62b1591a1220807c370d930f4a6b98f7bc
Patrick Yambrick <patrickyambrick@gmail.com>
2019-06-24 12:21:22 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv').config();
 const routes = require('./routes/basic');
 const path = require('path');
 const port = process.env.PORT || 3333;
-const environment = process.env.NODE_ENV || 'ls';
+const environment = process.env.NODE_ENV;
 
 let filePath;
 
@@ -25,9 +25,9 @@ app.use(cors(corsOption));
 app.use(express.static(path.join(__dirname, "client", filePath)))
 
 app.use('/api', routes);
-/*app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", 'public', "index.html"));
-});*/
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", 'build', "index.html"));
+});
 app.listen(port, () => {
 	console.log(`Your app is now running on port: ${port}`);
 })
